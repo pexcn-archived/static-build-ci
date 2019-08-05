aaab3a0db430d36f4cade32e4bd86ba82c202698
pexcn <i@pexcn.me>
2019-08-05 20:50:55 +0800

    update socks5-server to 1.8.7

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ SODIUM_URL=https://download.libsodium.org/libsodium/releases/libsodium-$SODIUM_V
 SS_LIBEV_VER=3.3.0
 SS_LIBEV_URL=https://github.com/shadowsocks/shadowsocks-libev/releases/download/v$SS_LIBEV_VER/shadowsocks-libev-$SS_LIBEV_VER.tar.gz
 
-SOCKS5_SERVER_VER=1.8.5
+SOCKS5_SERVER_VER=1.8.7
 SOCKS5_SERVER_URL=https://github.com/heiher/hev-socks5-server.git
 
 prepare() {
