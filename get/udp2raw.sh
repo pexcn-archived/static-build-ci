e422809d3474bacf83e138888cdacb933e9f5187
pexcn <i@pexcn.me>
2019-10-11 19:17:57 +0800

    fix udp2raw script

--- get/udp2raw.sh
+++ get/udp2raw.sh
@@ -4,13 +4,7 @@ TARGET_PATH="/usr/local/bin/udp2raw"
 API_URL="https://api.github.com/repos/wangyu-/udp2raw-tunnel/releases/latest"
 DOWNLOAD_URL=$(curl -sSL $API_URL | grep "browser_download_url" | grep "binaries" | cut -d '"' -f 4)
 
-curl -sSL $DOWNLOAD_URL | tar -zxf - -C /usr/local/bin/ $(_get_extract_name)
-mv $(_get_extract_name) $TARGET_PATH
-
-chmod +x $TARGET_PATH
-chown root:staff $TARGET_PATH
-
-_get_extract_name() {
+get_extract_name() {
   local aes_support=$(cat /proc/cpuinfo | grep "aes")
 
   if [ -z "$aes_support" ]; then
@@ -19,3 +13,9 @@ _get_extract_name() {
     echo "udp2raw_amd64_hw_aes"
   fi
 }
+
+curl -sSL $DOWNLOAD_URL | tar -zxf - -C /usr/local/bin/ $(get_extract_name)
+mv /usr/local/bin/$(get_extract_name) $TARGET_PATH
+
+chmod +x $TARGET_PATH
+chown root:staff $TARGET_PATH
